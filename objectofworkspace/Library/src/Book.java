ad6476579bbae4164cdc5c49ef81ceef5f84d118
HoJIN Park / 박호진 <parkhojin@naver.com>
2024-06-03 19:39:33 +0900

    6/3 2번문제

--- objectofworkspace/Library/src/Book.java
+++ objectofworkspace/Library/src/Book.java
@@ -1,5 +1,51 @@
 
 public class Book {
-	int number;
-	String title;
+	int number; // 관리번호
+	String title;	// 제목
+	String author;	// 저자
+	
+	// 기본 생성자
+	public Book() {
+		super();
+	}
+	// 세 가지를 모두 받는 생성자
+	public Book(int number, String title, String author) {
+		super();
+		this.number = number;
+		this.title = title;
+		this.author = author;
+	}
+	// 접속자 관리번호
+	public int getNumber() {
+		return number;
+	}
+	// 접속자 제목
+	public String getTitle() {
+		return title;
+	}
+	// 접속자 저자
+	public String getAuthor() {
+		return author;
+	}
+	// 설정자 관리번호
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	// 설정자 제목
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	// 설정자 저자
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Override
+	public String toString() {
+		return "Book [number=" + number + ", title=" + title + ", author=" + author + "]";
+	}
+	
+	// 특별히 출력에 대한 지시가 없다. 그래도 향후 관리를 위해 toString을 생성해 논다.
+	
+	
+	
 }
--- objectofworkspace/Library/src/Delinquency.java
+++ objectofworkspace/Library/src/Delinquency.java
@@ -1,4 +1,10 @@
 
 public interface Delinquency {
-
+	
+	 // 연체 가격
+	int DELAY_PAY_NOVEl = 300;// 소설 연체 가격
+	int DELAY_PAY_POET = 200;// 시집 연체 가격
+	int DELAY_PAY_SCIENCEFICTION = 600;// 공상과학 소설 연체 가격
+	
+	int getLastFees(); // 연체료를 계산하는 메소드 
 }
