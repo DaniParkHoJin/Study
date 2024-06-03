c550807a276c25eccadc4f3f4303080941a0741f
DaniParkHoJin <parkhojin@naver.com>
2024-06-03 23:11:00 +0900

    at home

--- objectofworkspace/BookMarket2/src/com/market/cart/CartInterface.java
+++ objectofworkspace/BookMarket2/src/com/market/cart/CartInterface.java
@@ -4,8 +4,8 @@ import com.market.bookitem.Book;
 
 public interface CartInterface {
 	void printBookList(Book[] p);// 전체 도서 정보 목록 출력
-	boolean isCartIntBook(String id);// 장바구니에 담긴 갯술를 1씩 증가
-	void issertBook(Book p);//CartItem에 도서 정보를 등록
+	boolean isCartInBook(String id);// 장바구니에 담긴 갯술를 1씩 증가
+	void insertBook(Book p);//CartItem에 도서 정보를 등록
 	void removeCart(int numId);// 장바구니 순번 numId의 항목을 삭제
 	void deleteBook();//장바구니 모든 항목을 삭제
 }
