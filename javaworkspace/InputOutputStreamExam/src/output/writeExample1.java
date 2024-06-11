28d93933161c9f9e1510f65c90ecca526e390107
HoJIN Park / 박호진 <parkhojin@naver.com>
2024-06-11 15:54:12 +0900

    파일 입출력 연습 중

---
+++ javaworkspace/InputOutputStreamExam/src/output/writeExample1.java
@@ -0,0 +1,28 @@
+package output;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class writeExample1 {
+
+	public static void main(String[] args) {
+		try {
+			OutputStream os = new FileOutputStream("D:/javaproject/javaworkspace/InputOutputStreamExam/Temp/test1.db");
+			byte a = 10;
+			byte b = 20;
+			byte c = 30;
+			
+			os.write(a);
+			os.write(b);
+			os.write(c);
+			
+			os.flush();
+			os.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
